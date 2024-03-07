ad7dae3bd022c5f623743852818fac4914886396
sujin <sujinsr2457@gmail.com>
2024-03-07 11:09:53 +0000

    Dataset cleaned

--- main.py
+++
@@ -1,6 +0,0 @@
-# Importing the libraries and Dependencies
-import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import Normalizer
-from sklearn.model_selection import GridSearchCV
